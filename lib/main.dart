d6c3f369f52cb6f3cd47acff7d5befb9ef74bb25
fruitbite <oneebzahid@gmail.com>
2021-11-25 10:48:01 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import '/screens/input_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color(0xFF0A0E21),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color(0xFF0A0E21),
+        ),
+      ),
+      home: const InputPage(),
+    );
+  }
+}
